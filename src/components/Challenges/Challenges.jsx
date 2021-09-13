7ed9529b2f68106e5a545872f7adde15bdd67617
suarezt3 <ey_der19@hotmail.com>
2021-09-13 17:03:14 -0500

    Se crean los botones

--- src/components/Challenges/Challenges.jsx
+++ src/components/Challenges/Challenges.jsx
@@ -4,7 +4,10 @@ import style from './Challenges.module.css'
 export const Challenges = () => {
     return (
         <div className={style.card_challenges}>
-            
+            <div className={style.box_img}>
+                <img src="" alt="" />
+            </div>
+            <button type="button"> <a href="https://reto-1-expanding-cards.netlify.app/" target="blank">Ver como funciona</a> </button>
         </div>
     )
 }
